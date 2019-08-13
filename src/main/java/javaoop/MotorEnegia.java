9b39852c964d919898442108ed8a9a140118a352
anlefinir <cristina.lefinir@mercadolibre.com>
2019-08-13 14:19:41 -0300

    se agrego otro espacio

--- src/main/java/javaoop/MotorEnegia.java
+++ src/main/java/javaoop/MotorEnegia.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class MotorEnegia extends Motor {
 
-    
+
     public void recibirRefrigerante(Refrigerante refrigerante){
         int x = (int) Math.round(refrigerante.intensidad * 0.2);
         temperatura -= x;
